183dd81e9d9f87accbc35f5b472cb21be133882c
reggietenk <reggietenkorang@gmail.com>
2022-01-06 20:23:27 -0500

    news page initial commit

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,3 +6,38 @@ var elem = document.getElementById("league-selections")
 var instance = M.FormSelect.getInstance(elem)
 
 var submitBtn = document.getElementById("league-submit-btn")
+
+
+//function gets input from form
+var getLeagueSelection = function(){
+  //selections from form
+  var input = document.getElementById("league-selections")
+  //selected option from form
+  var selection = input.value 
+  //run API with selected option 
+  leagueSearch(selection)
+
+}
+
+
+//function calls football standings API based on the league selection from form
+var leagueSearch = function (selection) {
+  var apiUrl = "https://api-football-standings.azharimm.site/leagues/"+selection+"/standings?season=2021&sort=asc"
+
+  fetch(apiUrl).then(function(response){
+      if (response.ok) {
+          response.json().then(function(data){
+              console.log(data)
+          })
+      } else {
+          console.log("Error: No data found")
+      }
+  })
+  .catch(function(error){
+      console.log("Unable to connect to API")
+  })
+}
+
+
+
+submitBtn.addEventListener("click", getLeagueSelection)
